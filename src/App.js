8920c6ca577b8e991e4ac347aed4ada03dcab412
iznor <iznor.hucuev9507@gmail.comm>
2023-01-23 12:50:35 +0300

    Сделал счетчик

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './index.scss';
-
+import React from 'react'
 function App() {
+
+
+  const [count, setCount] = React.useState(0) //начальное состояние счетчика
+
+
+  // функция для инкремента count 
+  const handleClikInc = () => { 
+    setCount(count => count + 1)
+  }
+ // функция для декремента count 
+  const handleClikDec = () => {
+    setCount(count => count - 1)
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button  disabled={count <= 0}  className="minus" onClick={() => handleClikDec()}>- Минус</button>
+        <button  className="plus" onClick={() => handleClikInc()}>Плюс +</button>
       </div>
     </div>
   );
